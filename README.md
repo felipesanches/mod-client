cb7854e1ccfec014763238c038c616cd5c8790ba
Bruno Gola <brunogola@gmail.com>
2014-09-17 18:56:41 +0300

    Documentation and scripts updated

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 mod-client
 ==========
 
-app to connect to the MOD webserver and access the web ui
+Instructions to run the MOD LV2 Host on any GNU/Linux
+
+Make sure you have Python 2.7 and PyQt4 installed:
+
+On Debian:
+$ apt-get install python-qt4
+
+On ArchLinux:
+$ pacman -S python2-pyqt4
+
+$ git clone --recursive http://github.com/portalmod/mod-client
+$ cd mod-client
+
+Debian users:
+$ virtualenv env
+$ ln -s /usr/lib/python2.7/dist-packages/PyQt4 env/lib/python2.7/site-packages/
+$ ln -s /usr/lib/python2.7/dist-packages/sip.so env/lib/python2.7/site-packages/
+
+ArchLinux users:
+$ virtualenv2 env
+$ ln -s /usr/lib/python2.7/site-packages/PyQt4 env/lib/python2.7/site-packages/
+$ ln -s /usr/lib/python2.7/site-packages/sip.so env/lib/python2.7/site-packages/
+
+$ . env/bin/activate
+$ pip install -e requirements.txt --allow-unverified PIL --allow-external PIL
+$ cd mod-ui/
+$ python setup.py install
+$ cd ..
+$ cd mod-host
+$ make
+$ cd ..
+$ ./mod-lv2-host.py --scan-lv2
+This may take some time, while MOD software indexes information about your LV2 plugins
+
+$ ./mod-lv2-host.py
--- mod-lv2-host.py
+++ mod-lv2-host.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
 
-from os.path import join
+from os.path import join, realpath
 
 import os, sys
 import subprocess
 
 MOD_ADDR="http://127.0.0.1:7988"
 
-DATA_DIR = join(os.environ['HOME'], '.mod-lv2-host/data')
+ROOT = os.path.dirname(realpath(sys.argv[0]))
+
+DATA_DIR = join(ROOT, 'data')
 
 os.environ['MOD_DEV_HOST'] = "0"
 os.environ['MOD_DEV_HMI'] = "1"
 os.environ['MOD_DATA_DIR'] = DATA_DIR
 os.environ['MOD_DESKTOP'] = "1"
 os.environ['MOD_LOG'] = "0"
-os.environ['MOD_HTML_DIR'] = join(".", 'html')
+os.environ['MOD_HTML_DIR'] = join(ROOT, 'mod-ui/html')
+os.environ['MOD_PLUGIN_LIBRARY_DIR'] = join(os.environ['HOME'], ".lv2")
 os.environ['MOD_DEVICE_WEBSERVER_PORT'] = '7988'
-os.environ['MOD_PHANTOM_BINARY'] = join(".", 'phantomjs-1.9.0-linux-x86_64/bin/phantomjs')
-os.environ['MOD_SCREENSHOT_JS'] = join(".", 'screenshot.js')
+os.environ['MOD_PHANTOM_BINARY'] = join(ROOT, 'phantomjs-1.9.0-linux-x86_64/bin/phantomjs')
+os.environ['MOD_SCREENSHOT_JS'] = join(ROOT, 'screenshot.js')
 
-#sys.path = sys.path
-
-if len(sys.argv) > 1 and sys.argv[1] == "--scan-lv2":
-    from mod import rebuild_database
-    rebuild_database()
-    sys.exit(0)
+if len(sys.argv) > 1:
+    if sys.argv[1] == "--scan-lv2":
+        print "Scanning and indexing your LV2 plugins...",
+        from mod import rebuild_database
+        rebuild_database()
+        print "done"
+        sys.exit(0)
+    else:
+        print "Usage: %s [--scan-lv2]" % sys.argv[0]
+        sys.exit(2)
 
 def jack_not_running():
     p = subprocess.Popen(['jack_wait', '-c'], stdout=subprocess.PIPE)
@@ -39,9 +46,9 @@ from mod import webserver
 from multiprocessing import Process
 
 # run mod-ui and mod-host
-os.system("./mod-host/mod-host")
-p = Process(target=webserver.run)
-p.start()
+host = subprocess.Popen([join(ROOT, "mod-host/mod-host")])
+ui = Process(target=webserver.run)
+ui.start()
 
 # start a PyQt webkit
 from PyQt4.QtCore import *
@@ -52,4 +59,8 @@ app = QApplication(sys.argv)
 web = QWebView()
 web.load(QUrl(MOD_ADDR))
 web.show()
-sys.exit(app.exec_())
+r = app.exec_()
+ui.terminate()
+host.kill()
+os.system("pkill mod-host")
+sys.exit(r)
